10a3cf2377f986394a2499ec12a84eb64ad68c4a
chc-scripts <119128091+chc-scripts@users.noreply.github.com>
2022-11-30 12:53:33 +0100

    Add files via upload

--- Caracteres_Speciaux/Caracteres_Speciaux.jsx
+++ Caracteres_Speciaux/Caracteres_Speciaux.jsx
@@ -3,7 +3,6 @@
 >=-----------------------------------------------------------------------------------------------------------------------------
 Author: Christian Condamine - (christian.condamine@laposte.net)
 >=-----------------------------------------------------------------------------------------------------------------------------
-
 Affiche une palette flottante qui permet, lors de la saisie de texte dans Illustrator, d’accéder
 facilement à des caractères spéciaux fréquemment utilisés en français mais qui ne sont
 pas directement accessibles, comme, par exemple, certaines majuscules accentuées.
@@ -41,49 +40,39 @@ if (maSelection.typename != 'TextRange') {
         // ==Boutons
         var btn_1 = boiteDial.add("button",[0,0,25,25],"\300"); 
             btn_1.onClick = function() {btExecution('monCar = "\300"');
-                                                       insererCar(idx);
-                                                       btn_1.active=false;};
+                                                       insererCar();btn_1.active=false;};
         var btn_2 = boiteDial.add("button",[0,0,25,25],"\311"); 
             btn_2.onClick = function() {btExecution('monCar = "\311"');
-                                                       insererCar(idx);
-                                                       btn_2.active=false;};
+                                                       insererCar();btn_2.active=false;};
         var btn_3 = boiteDial.add("button",[0,0,25,25],"\310"); 
             btn_3.onClick = function() {btExecution('monCar = "\310"')
-                                                       insererCar(idx)
-                                                       btn_3.active=false;};
+                                                       insererCar();btn_3.active=false;};
         var btn_4 = boiteDial.add("button",[0,0,25,25],"\331"); 
             btn_4.onClick = function() {btExecution('monCar = "\331"')
-                                                       insererCar(idx)
-                                                       btn_3.active=false;};
+                                                       insererCar();btn_3.active=false;};
         var btn_5 = boiteDial.add("button",[0,0,25,25],"\307"); 
             btn_5.onClick = function() {btExecution('monCar = "\307"');
-                                                       insererCar(idx);
-                                                       btn_5.active=false;};
+                                                       insererCar();btn_5.active=false;};
         var btn_6 = boiteDial.add("button",[0,0,25,25],"\306"); 
             btn_6.onClick = function() {btExecution('monCar = "\306"');
-                                                       insererCar(idx);
-                                                       btn_6.active=false;};
+                                                       insererCar();btn_6.active=false;};
         var btn_7 = boiteDial.add("button",[0,0,25,25],"\u0152"); 
-            //btn_7.graphics.font = "Tahoma-Bold:18";
             btn_7.onClick = function() {btExecution('monCar = "\u0152"');
-                                                       insererCar(idx);
-                                                       btn_7.active=false;};
+                                                       insererCar();btn_7.active=false;};
         var btn_8 = boiteDial.add("button",[0,0,25,25],"\346"); 
             btn_8.onClick = function() {btExecution('monCar = "\346"');
-                                                       btn_8.active=false;};
+                                                       insererCar();btn_8.active=false;};
         var btn_9 = boiteDial.add("button",[0,0,25,25],"\u0153"); 
             btn_9.onClick = function() {btExecution('monCar = "\u0153"');
-                                                       insererCar(idx);
-                                                       btn_9.active=false;};
+                                                       insererCar();btn_9.active=false;};
         var btn_10 = boiteDial.add("button",[0,0,25,25],"\330"); 
             btn_10.onClick = function() {btExecution('monCar = "\330"');
-                                                       insererCar(idx);
-                                                       btn_10.active=false;};
+                                                       insererCar();btn_10.active=false;};
 for (f = 1;f<11;f++){("btn_" + f).font = "Tahoma-Bold:18";};
 boiteDial.show();
 boiteDial.f
 };
-function insererCar($ype) { 
+function insererCar() { 
 btExecution('maSelection = app.activeDocument.selection');
 btExecution('maSelection.contents = monCar');
 };
